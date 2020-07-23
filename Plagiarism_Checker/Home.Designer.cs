87ff983d64dfa0c57f62f0e80908ffcdbc49d88a
Muhammad Ahmad <56221034+muhammad-ahmad1@users.noreply.github.com>
2020-07-23 16:40:51 +0500

    done

--- Plagiarism_Checker/Home.Designer.cs
+++ Plagiarism_Checker/Home.Designer.cs
@@ -46,9 +46,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(149, 208);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.Size = new System.Drawing.Size(160, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "File Type";
+            this.label2.Text = "ni chrna bhai kaam he krraha hu";
             // 
             // Home
             // 
